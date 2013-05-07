fed6109b647916330cbc0649ae53c5f199ad12b3
m4rw3r <m4rw3r@gmail.com>
2013-05-07 19:43:37 +0200

    Added Apache support in the DefaultRequestInfoProvider

--- RequestInfoProvider/DefaultRequestInfoProvider.php
+++ RequestInfoProvider/DefaultRequestInfoProvider.php
@@ -10,7 +10,20 @@ class DefaultRequestInfoProvider implements RequestInfoProviderInterface
 {
 	public function getAuthorizationHeader(Request $req)
 	{
-		return $req->headers->has('authorization') ? $req->headers->get('authorization') : false;
+		if($req->headers->has('authorization')) {
+			return $req->headers->get('authorization');
+		}
+		
+		/* Apache gobbles up the authorization header, so we need to 
+		 * manually retrieve it.
+		 */
+		if(function_exists('apache_request_headers')) {
+			$headers = apache_request_headers();
+			
+			return empty($headers['Authorization']) ? false : $headers['Authorization'];
+		}
+		
+		return false;
 	}
 	public function getBody(Request $req)
 	{
